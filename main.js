674e94514cdbf53daf6e202c5dc7e24c06749bbe
RafaelB <rafaelboschmamail@gmail.com>
2022-05-19 22:56:47 +0200

    Finished so the game actually checks if the game has ended and announces the winner

--- main.js
+++ main.js
@@ -22,13 +22,19 @@ const gameBoard = (function() {
     gameTiles.forEach(tile => {
       tile.addEventListener('click', _clickListener);
     })
-  }
+  };
+
+  // Removes click listeners from all tiles
+  function removeClickListeners() {
+    gameTiles.forEach(tile => {
+      tile.removeEventListener('click', _clickListener);
+    });
+  };
 
   // Changes the tile to show the players marker
   function _clickListener(e) {
     const turnCheckResult = game.turnCheck(e);
     if (turnCheckResult === false) return;
-    console.log(turnCheckResult);
     if (turnCheckResult === 'X') {
       gameBoard[e.target.id] = 'X';
     } else {
@@ -36,12 +42,77 @@ const gameBoard = (function() {
     };
     render();
     game.switchTurn();
+    game.checkGameOver()
+  };
+
+  // Checks if X won
+  function checkXWin() {
+    const g = gameBoard;
+    if (g[0] === 'X' && g[1] === 'X' && g[2] === 'X') {
+      return true;
+    } else if (g[3] === 'X' && g[4] === 'X' && g[5] === 'X') {
+      return true;
+    } else if (g[6] === 'X' && g[7] === 'X' && g[8] === 'X') {
+      return true;
+    } else if (g[0] === 'X' && g[3] === 'X' && g[6] === 'X') {
+      return true;
+    } else if (g[1] === 'X' && g[4] === 'X' && g[7] === 'X') {
+      return true;
+    } else if (g[2] === 'X' && g[5] === 'X' && g[8] === 'X') {
+      return true;
+    } else if (g[0] === 'X' && g[4] === 'X' && g[8] === 'X') {
+      return true;
+    } else if (g[2] === 'X' && g[4] === 'X' && g[6] === 'X') {
+      return true;
+    };
+  };
+
+  // Checks if O won
+  function checkOWin() {
+    const g = gameBoard;
+    if (g[0] === 'O' && g[1] === 'O' && g[2] === 'O') {
+      return true;
+    } else if (g[3] === 'O' && g[4] === 'O' && g[5] === 'O') {
+      return true;
+    } else if (g[6] === 'O' && g[7] === 'O' && g[8] === 'O') {
+      return true;
+    } else if (g[0] === 'O' && g[3] === 'O' && g[6] === 'O') {
+      return true;
+    } else if (g[1] === 'O' && g[4] === 'O' && g[7] === 'O') {
+      return true;
+    } else if (g[2] === 'O' && g[5] === 'O' && g[8] === 'O') {
+      return true;
+    } else if (g[0] === 'O' && g[4] === 'O' && g[8] === 'O') {
+      return true;
+    } else if (g[2] === 'O' && g[4] === 'O' && g[6] === 'O') {
+      return true;
+    };
+  };
+
+  // Checks if the game has ended in a tie
+  function checkForTie() {
+    let num = 0;
+    gameBoard.forEach(tile => {
+      if (tile === 'X' || tile === 'O') {
+        return num++;
+      } else {
+        return;
+      };
+    });
+    if (num === 9) {
+      return true;
+    };
   };
 
   _addClickListeners();
 
   return {
-    render
+    gameBoard,
+    render,
+    checkXWin,
+    checkOWin,
+    checkForTie,
+    removeClickListeners
   };
 })();
 
@@ -75,11 +146,32 @@ const game = (function() {
     };
   }
 
+  // Checks if the game is over
+  function checkGameOver() {
+    const checkX = gameBoard.checkXWin();
+    const checkO = gameBoard.checkOWin();
+    const checkTie = gameBoard.checkForTie();
+    if (checkX === true) {
+      gameBoard.removeClickListeners();
+      gameInfoText.innerHTML = `${player1.getName()} won!`
+      return true;
+    } else if (checkO === true) {
+      gameBoard.removeClickListeners();
+      gameInfoText.innerHTML = `${player2.getName()} won!`
+      return true;
+    } else if (checkTie === true) {
+      gameBoard.removeClickListeners();
+      gameInfoText.innerHTML = "It's a tie!"
+      return true;
+    };
+  };
+
   return {
     player1,
     player2,
     turn,
     turnCheck,
-    switchTurn
+    switchTurn,
+    checkGameOver
   };
 })();
